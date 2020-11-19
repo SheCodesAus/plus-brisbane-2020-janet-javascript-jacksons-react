6750691336746d6a839e354cbabe4b47e25ed934
Joy Taylor <jcat7414@gmail.com>
2020-11-19 18:30:41 +1000

    Debug db fetch

--- src/pages/EventsPage.jsx
+++ src/pages/EventsPage.jsx
@@ -9,12 +9,11 @@ function EventsPage() {
         fetch(`${process.env.REACT_APP_API_URL}events/`, {
             headers: {
                 "Content-Type": "application/json",
-                // "Accept": "application/json"
+                "Accept": "application/json"
             },
         })
         .then((results) => {
-            // return results.json();
-            console.log(results)
+            return results.json();
         })
         .then((data) => {
             setEventList(data);
@@ -26,9 +25,11 @@ function EventsPage() {
             <Link to='/new'><button type="button">Create an Event</button></Link>
             <div>
                 <h1>Events</h1>
-                {/* {eventList.map((eventData, key) => { */}
-                    {/* return <EventCard key={key} eventData={eventData} />; */}
-                {/* })} */}
+            </div>
+            <div>
+                {eventList.map((eventData, key) => {
+                    return <EventCard key={key} eventData={eventData} />;
+                })}
             </div>
         </div>
     );
