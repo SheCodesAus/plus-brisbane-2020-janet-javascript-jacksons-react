70cf01444176d23cd82b61ff1f2b11fd321bed26
vivianqutau <36681929+vivianqutau@users.noreply.github.com>
2020-11-29 22:01:53 +1000

    update css

--- src/components/Nav/Footer.jsx
+++ src/components/Nav/Footer.jsx
@@ -1,12 +1,11 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import './Footer.css';
 
 function Footer() {
     return (
-        <nav>
-            <div className="footer">
-                <Link to="/about">About</Link>
-                &nbsp;&nbsp; | &nbsp;&nbsp;
+        <footer>
+            <div className="social-medias">
                 <Link to="/twitter">Twitter</Link>
                 &nbsp;&nbsp; | &nbsp;&nbsp;
                 <Link to="/contact">Contact</Link>
@@ -14,7 +13,10 @@ function Footer() {
             <div className="copyright">
                 <p>&copy; Deep Tech Heroes 2020</p>
             </div>
-        </nav>
+            <div className="trade-mark">
+                <Link to="/about">Site created by J-cubed</Link>
+            </div>
+        </footer>
     );
 }
 
