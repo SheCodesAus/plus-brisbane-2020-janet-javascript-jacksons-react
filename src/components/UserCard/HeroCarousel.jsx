d21b1cef18d27c40f01a513e7984652cb65588d6
Joy Taylor <jcat7414@gmail.com>
2020-11-29 11:35:29 +1000

    Add Hero Carousel and Host Carousel to Home Page

---
+++ src/components/UserCard/HeroCarousel.jsx
@@ -0,0 +1,31 @@
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import Carousel from "react-elastic-carousel";
+import HeroCard from "./HeroCard";
+
+function HeroCarousel() {
+    const [heroList, setHeroList] = useState([]);
+
+    useEffect(() => {
+        fetch(`${process.env.REACT_APP_API_URL}users/heros`, {
+            headers: {
+                "Content-Type": "application/json",
+                "Accept": "application/json"
+            },
+        })
+        .then((results) => {
+            return results.json();
+        })
+        .then((data) => {
+            setHeroList(data);
+        });
+    }, []);
+
+    return (
+        <Carousel>
+            {heroList.map(heroData => <div key={heroData.id}><HeroCard heroData={heroData}/></div>)}
+        </Carousel>
+    );
+}
+
+export default HeroCarousel;
