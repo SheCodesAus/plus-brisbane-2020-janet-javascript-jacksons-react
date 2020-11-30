69d5d61f95c180a77f41623178b6c726a606818e
vivianqutau <36681929+vivianqutau@users.noreply.github.com>
2020-11-30 23:58:07 +1000

    Form styling

--- src/pages/HeroesPage.jsx
+++ src/pages/HeroesPage.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import HeroCard from '../components/UserCard/HeroCard';
+import "./EventsPage.css";
 
 
 function HeroesPage() {
