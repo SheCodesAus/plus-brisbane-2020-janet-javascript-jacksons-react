4be90c93177a970641eb6b9cde056e8b56198cd5
Atiyeh-code <atiyeh.vaezipour@gmail.com>
2020-11-30 12:13:10 +1000

    EventPage Css

--- src/pages/EventPage.jsx
+++ src/pages/EventPage.jsx
@@ -9,6 +9,7 @@ import SkillsExpert from '../components/EventCard/SkillsExpert';
 import SkillsEnthusiast from '../components/EventCard/SkillsEnthusiast';
 import PaidProbono from '../components/EventCard/PaidProbono';
 import EventSize from '../components/EventCard/EventSize';
+import "./EventPage.css";
 
 function EventPage() {
     const [eventData, setEventData] = useState({ events: [], applications: [], review_event: [] });
@@ -35,29 +36,32 @@ function EventPage() {
     window.localStorage.setItem("host_username", eventData.host_username);
 
     return (
-        <div>
+        <div className="page-wrapper">
             <div className='event-title'>
                 <h1>{ eventData.event_name }</h1>
             </div>
-            <div className='event-image'>
-                <img src={ eventData.image } alt="" />
-            </div>
-            <div className='event-description'>
-                <h5>Location: { eventData.event_location }</h5>
-                <p>Date: { eventData.event_date } </p>
-                <p>Time: { eventData.event_time } </p>
-                <p>Details: { eventData.event_description }</p>
-                {/* We are expecting this to be a: <EventSize eventData={eventData} />              */}
-                <p>The Event Host is seeking Heroes for the following opportunities: </p>
-                <SkillsKeynote eventData={eventData} />
-                <SkillsFacilitator eventData={eventData} />
-                <SkillsMentor eventData={eventData} />
-                <SkillsExpert eventData={eventData} />
-                <SkillsEnthusiast eventData={eventData} />
-                <PaidProbono eventData={eventData} />
-                <br/>
-                <p>Are you interested in being a Hero for this Event?</p>
-                <Link to="/apply"><button type='button'>Apply to be a Hero</button></Link>
+
+            <div className="event-wrapper">
+                <div className='event-image'>
+                    <img src={ eventData.image } alt="" />
+                </div>
+                <div className='event-description'>
+                    <h5>Location: { eventData.event_location }</h5>
+                    <p>Date: { eventData.event_date } </p>
+                    <p>Time: { eventData.event_time } </p>
+                    <p>Details: { eventData.event_description }</p>
+                    {/* We are expecting this to be a: <EventSize eventData={eventData} />              */}
+                    <p>The Event Host is seeking Heroes for the following opportunities: </p>
+                    <SkillsKeynote eventData={eventData} />
+                    <SkillsFacilitator eventData={eventData} />
+                    <SkillsMentor eventData={eventData} />
+                    <SkillsExpert eventData={eventData} />
+                    <SkillsEnthusiast eventData={eventData} />
+                    <PaidProbono eventData={eventData} />
+                    <br/>
+                    <p>Are you interested in being a Hero for this Event?</p>
+                    <Link to="/apply"><button type='button'>Apply to be a Hero</button></Link>
+                </div >
             </div>
             <br/>
             <div>
